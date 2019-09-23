57ceb04954c52de04c630b3e20fad9a396b40400
秦Daniel <daniel_chin@yahoo.com>
2019-09-23 16:48:26 -0400

    rendering digit

--- render.m
+++ render.m
@@ -6,19 +6,27 @@ mesh(GROUND_X, GROUND_Y, GROUND_Z);
 % draw edges
 for pair = EDGE
     absolute = X(pair, :) + CM_position;
-    line(absolute(:, 1), absolute(:, 2), absolute(:, 3), 'linewidth', sum(X(pair, 3)) + 3);
+    if any(absolute(:, 3) < 0)
+        color = 'red';
+    else
+        color = 'black';
+    end
+    line( ...
+        absolute(:, 1), absolute(:, 2), absolute(:, 3), ...
+        'linewidth', sum(X(pair, 3)) + 3, ...
+        'color'    , color ...
+    );
 end
 
-% draw dots
+% draw number on face
 for face = 1:6
-    base = X(FACE_CORNER(1, [2 3]), :) - X(FACE_CORNER(1, [1 1]), :);
-    offset = CM_position + X(FACE_CORNER(1, 1), :);
-    for dot_i = 1:face
-        % viscircles( ...
-            % offset + FACE_DOT(face, :, dot_i) * base, ...
-            % DOT_SIZE(face) ...
-        % );
-    end
+    base = X(FACE_CORNER(face, [2 3]), :) - X(FACE_CORNER(face, [1 1]), :);
+    offset = CM_position + X(FACE_CORNER(face, 1), :);
+    path = offset + FACE_MARK(:, :, face) * base;
+    line(path(:, 1), path(:, 2), path(:, 3), ...
+        'color', FACE_COLOR(face, :), ...
+        'linewidth', 2 ...
+    );
 end
 
 % lock 3d view
