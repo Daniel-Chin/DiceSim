5e3f8145c97f6e0ef44ac42d2c409fed69279653
秦Daniel <daniel_chin@yahoo.com>
2019-09-25 20:51:56 -0400

    rollAndRender now shows the end

--- rollAndRender.m
+++ rollAndRender.m
@@ -1,9 +1,13 @@
 step();
 T = delta_t;
-while totalEnergy(point_v, absolute_X_3, press_force, - GRAVITY(3), theoratical_energy_zero) > confirm_destiny_energy
+i = 20;
+while i > 0
     step(); step(); step(); step(); step(); step(); step(); step(); 
     T = T + delta_t * 8;
     render();
     pause(.001);
+    if totalEnergy(point_v, absolute_X_3, press_force, - GRAVITY(3), theoratical_energy_zero) < confirm_destiny_energy
+        i = i - 1;  % just to show the end
+    end
 end
 roll_result = whichFace(X, FACE_LOOKUP);
