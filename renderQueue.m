0ac40ee380432e6f8b5b397ed52deda73f2a3514
Daniel Chin <daniel_chin@yahoo.com>
2019-10-07 16:35:30 -0400

    new render queue

--- renderQueue.m
+++ renderQueue.m
@@ -1,53 +1,62 @@
 initProgram();
 loadDice();
-x = .01978; y = .018265;
+x = 1.65; y = 3.9;
 initDiceFromXY();
-VIDEO_DIR = 'video/MicroParty_all_color/b';%
+VIDEO_DIR = 'video/red_sea/right';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR '/' 'b_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'right_%d.png']
 rollAndSaveVideo();
 
 initProgram();
 loadDice();
-x = .01981; y = .0182;
+x = 1.5; y = 4;
 initDiceFromXY();
-VIDEO_DIR = 'video/MicroParty_all_color/k';%
+VIDEO_DIR = 'video/red_sea/mid';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR '/' 'k_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'mid_%d.png']
 rollAndSaveVideo();
 
 initProgram();
 loadDice();
-x = .01979; y = .01823;
+x = 1.2; y = 4.4;
 initDiceFromXY();
-VIDEO_DIR = 'video/MicroParty_all_color/y';%
+VIDEO_DIR = 'video/red_sea/left';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR '/' 'y_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'left_%d.png']
 rollAndSaveVideo();
 
 initProgram();
 loadDice();
-x = .01979; y = .01827;
+x = 0; y = 0;
 initDiceFromXY();
-VIDEO_DIR = 'video/MicroParty_all_color/v';%
+VIDEO_DIR = 'video/zoom_out/x0y0';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR '/' 'v_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'x0y0_%d.png']
 rollAndSaveVideo();
 
 initProgram();
 loadDice();
-x = .01978; y = .018237;
+x = 1; y = 0;
 initDiceFromXY();
-VIDEO_DIR = 'video/MicroParty_all_color/r';%
+VIDEO_DIR = 'video/zoom_out/x1y0';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR '/' 'r_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'x1y0_%d.png']
 rollAndSaveVideo();
 
 initProgram();
 loadDice();
-x = .01981; y = .018265;
+x = 0; y = 1;
 initDiceFromXY();
-VIDEO_DIR = 'video/MicroParty_all_color/g';%
+VIDEO_DIR = 'video/zoom_out/x0y1';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR '/' 'g_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'x0y1_%d.png']
+rollAndSaveVideo();
+
+initProgram();
+loadDice();
+x = 1; y = 1;
+initDiceFromXY();
+VIDEO_DIR = 'video/zoom_out/x1y1';%
+mkdir(VIDEO_DIR);
+VIDEO_PATH = [VIDEO_DIR '/' 'x1y1_%d.png']
 rollAndSaveVideo();
