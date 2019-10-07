0e852099314cd549bf8d69cd634e803710fad3db
Daniel Chin <daniel_chin@yahoo.com>
2019-10-07 15:28:09 -0400

    AE project starting; renderQueue: micro party all colors

--- renderQueue.m
+++ renderQueue.m
@@ -1,26 +1,53 @@
 initProgram();
 loadDice();
-cursor = 0.0037191;
-initDiceFromCursor();
-VIDEO_DIR = 'video/border/right/';%
+x = .01978; y = .018265;
+initDiceFromXY();
+VIDEO_DIR = 'video/MicroParty_all_color/b';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR 'd_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'b_%d.png']
 rollAndSaveVideo();
 
 initProgram();
 loadDice();
-cursor = .00810637;
-initDiceFromCursor();
-VIDEO_DIR = 'video/TiaoKong/left/';%
+x = .01981; y = .0182;
+initDiceFromXY();
+VIDEO_DIR = 'video/MicroParty_all_color/k';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR 'd_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'k_%d.png']
 rollAndSaveVideo();
 
 initProgram();
 loadDice();
-cursor = .00810639;
-initDiceFromCursor();
-VIDEO_DIR = 'video/TiaoKong/right/';%
+x = .01979; y = .01823;
+initDiceFromXY();
+VIDEO_DIR = 'video/MicroParty_all_color/y';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR 'd_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'y_%d.png']
+rollAndSaveVideo();
+
+initProgram();
+loadDice();
+x = .01979; y = .01827;
+initDiceFromXY();
+VIDEO_DIR = 'video/MicroParty_all_color/v';%
+mkdir(VIDEO_DIR);
+VIDEO_PATH = [VIDEO_DIR '/' 'v_%d.png']
+rollAndSaveVideo();
+
+initProgram();
+loadDice();
+x = .01978; y = .018237;
+initDiceFromXY();
+VIDEO_DIR = 'video/MicroParty_all_color/r';%
+mkdir(VIDEO_DIR);
+VIDEO_PATH = [VIDEO_DIR '/' 'r_%d.png']
+rollAndSaveVideo();
+
+initProgram();
+loadDice();
+x = .01981; y = .018265;
+initDiceFromXY();
+VIDEO_DIR = 'video/MicroParty_all_color/g';%
+mkdir(VIDEO_DIR);
+VIDEO_PATH = [VIDEO_DIR '/' 'g_%d.png']
 rollAndSaveVideo();
