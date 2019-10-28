426a43b628067b2c56dbc9c9feace5c6d86b77a7
Daniel Chin <daniel_chin@yahoo.com>
2019-10-28 17:16:04 -0400

    explaining

---
+++ topologyOfMicroPartyYellows.m
@@ -0,0 +1,19 @@
+initProgram();
+
+loadDice();
+x = .019775; y = .0182;
+initDiceFromXY();
+rollAndTopology();
+RR = ground_contact_history;
+
+loadDice();
+x = .01979; y = .01824;
+initDiceFromXY();
+rollAndTopology();
+MM = ground_contact_history;
+
+loadDice();
+x = .0198; y = .01827;
+initDiceFromXY();
+rollAndTopology();
+LL = ground_contact_history;
