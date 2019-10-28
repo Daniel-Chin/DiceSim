75de992473aba144819427fd6097af32d4795772
Daniel Chin <daniel_chin@yahoo.com>
2019-10-28 16:00:16 -0400

    writing wrtieup

--- renderQueue.m
+++ renderQueue.m
@@ -1,8 +1,26 @@
 initProgram();
 loadDice();
-x = 1.65; y = 3.9;
+x = .019775; y = .0182;
 initDiceFromXY();
-VIDEO_DIR = 'video/red_sea/right';%
+VIDEO_DIR = 'video/micro_yells/down';%
 mkdir(VIDEO_DIR);
-VIDEO_PATH = [VIDEO_DIR '/' 'right_%d.png']
+VIDEO_PATH = [VIDEO_DIR '/' 'down_%d.png']
+rollAndSaveVideo();
+
+initProgram();
+loadDice();
+x = .01979; y = .01824;
+initDiceFromXY();
+VIDEO_DIR = 'video/micro_yells/mid';%
+mkdir(VIDEO_DIR);
+VIDEO_PATH = [VIDEO_DIR '/' 'mid%d.png']
+rollAndSaveVideo();
+
+initProgram();
+loadDice();
+x = .0198; y = .01827;
+initDiceFromXY();
+VIDEO_DIR = 'video/micro_yells/up';%
+mkdir(VIDEO_DIR);
+VIDEO_PATH = [VIDEO_DIR '/' 'up%d.png']
 rollAndSaveVideo();
--- scatterScript.m
+++ scatterScript.m
@@ -1,8 +1,21 @@
 initProgram;
-x_range = [.003719008 .003719011];
-RESOLUTION = 0.00000000002;
-SCATTER_DIR = '1D_scatter/';%
+GROUND_DAMP = 0.005;  % in s/cm
+HONEST = true;
+T_AXIS = false;
+x_range = [0 .1]; y_range = [0 .1];
+SCATTER_DIR = '2D_scatter/damp/mid/';%
 mkdir(SCATTER_DIR);
-SCATTER_PATH = [SCATTER_DIR 'border_5.png'];%
-investigate1D_dumb();
+SCATTER_PATH = [SCATTER_DIR 'd_%d.png'];%
+investigate2D();
+saveas(gcf, SCATTER_PATH);
+
+initProgram;
+GROUND_DAMP = 0.01;  % in s/cm
+HONEST = true;
+T_AXIS = false;
+x_range = [0 .1]; y_range = [0 .1];
+SCATTER_DIR = '2D_scatter/damp/high/';%
+mkdir(SCATTER_DIR);
+SCATTER_PATH = [SCATTER_DIR 'd_%d.png'];%
+investigate2D();
 saveas(gcf, SCATTER_PATH);
